caef87b45fb620590625e2f66ddc4b71bd7afedc
Connor Haaf <connorhaaf@mac.lan>
2026-05-29 19:24:18 -0400

    Tighten OCI deploy file handling

--- terraform/oci/versions.tf
+++ terraform/oci/versions.tf
@@ -13,7 +13,7 @@ provider "oci" {
   auth                = var.auth
   config_file_profile = var.config_file_profile
   fingerprint         = var.fingerprint
-  private_key_path    = var.private_key_path
+  private_key_path    = pathexpand(var.private_key_path)
   region              = var.region
   tenancy_ocid        = var.tenancy_ocid
   user_ocid           = var.user_ocid
